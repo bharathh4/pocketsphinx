afb80d167c79e37586e286a64c7af985e45455a9
Bharath <bharathh4@gmail.com>
2015-05-18 14:58:57 -0500

    Removed all comments written for testing purposes

--- app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -72,9 +72,7 @@ public class PocketSphinxActivity extends Activity implements
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
-        // Added a comment
         // Prepare the data for UI
-        // One more comment
         captions = new HashMap<String, Integer>();
         captions.put(KWS_SEARCH, R.string.kws_caption);
         captions.put(MENU_SEARCH, R.string.menu_caption);
